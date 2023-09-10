fb0c25f9162bd64c7fb759cc38a3238b5a3a5079
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-09-11 00:26:40 +0330

    solve conflicts in App.jsx
    I wroten some codes to solve conflicts

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Header from "./components/header/Header"
+import { Outlet } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div className="flex flex-col">
-      <Header/>
-      <Outlet/>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
