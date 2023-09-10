322475f76f0063664edb6510e515c1af695d6282
Bolori <109837376+Mhboolor@users.noreply.github.com>
2023-09-10 11:58:31 +0330

    feat : create & config redux store

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./global.css";
 
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
