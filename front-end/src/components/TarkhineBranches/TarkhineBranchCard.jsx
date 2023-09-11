84522abb0efdfa2146d0829fe85f96d5cd017b4a
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2023-09-11 12:49:44 +0330

    feat: create TarkhineBranches and TarkhineBranchCard Components

---
+++ front-end/src/components/TarkhineBranches/TarkhineBranchCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TarkhineBranchCard = () => {
+  return (
+    <div>TarkhineBranchCard</div>
+  )
+}
+
+export default TarkhineBranchCard
---
+++ front-end/src/components/TarkhineBranches/TarkhineBranches.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TarkhineBranchCard from './TarkhineBranchCard'
+
+const TarkhineBranches = () => {
+    return (
+        <section>
+            <h6>ترخینه گردی</h6>
+            <TarkhineBranchCard />
+            <TarkhineBranchCard />
+            <TarkhineBranchCard />
+            <TarkhineBranchCard />
+        </section>
+    )
+}
+
+export default TarkhineBranches
