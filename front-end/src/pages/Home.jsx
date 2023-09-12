e2f3ee9feec3464a5db593b0f1a3e2957c5eb020
Mhboolor <mhstare81@gmail.com>
2023-09-12 14:59:23 +0330

    feat : create 404 error component

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import HeroSlider from "../components/common/heroSlider/HeroSlider"
+import SearchBox from "../components/common/searchBox/SearchBox"
+
 function Home() {
   return (
-    <div>
-      Home
+    <div className="flex flex-col gap-5">
+      <HeroSlider/>
+      <SearchBox/>
     </div>
   )
 }
